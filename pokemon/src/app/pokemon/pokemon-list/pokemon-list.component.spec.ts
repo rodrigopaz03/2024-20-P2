0bddcf1a1bd6237d08fe51e16bb712539e5f0ba7
rodrigopaz03 <r.pazl@uniandes.edu.co>
2024-11-21 20:49:11 -0500

    cambios iniciales

--- pokemon/src/app/pokemon/pokemon-list/pokemon-list.component.spec.ts
+++ pokemon/src/app/pokemon/pokemon-list/pokemon-list.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('PokemonListComponent', () => {
   let component: PokemonListComponent;
   let fixture: ComponentFixture<PokemonListComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PokemonListComponent ]
     })
